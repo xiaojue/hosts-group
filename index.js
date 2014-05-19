4c538638d0ad43ad305cdb6bcd676d4e77d574a9
xiaojue <designsor@gmail.com>
2014-05-19 17:25:22 +0800

    hosts 互斥，只有一个可以生效

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ var blankReg = /\s+/;
 var notes = {};
 
 function hosts(defaultName) {
-    this.defaultName = defaultName || 'defaultGroup';
+	this.defaultName = defaultName || 'defaultGroup';
 	this.format();
 }
 /**
@@ -44,6 +44,17 @@ hosts
 */
 hosts.prototype = {
 	constructor: hosts,
+	//新增方法，避免互斥，修改domain之前，先把所有domain都禁用
+	_disableAllDomain:function(domain,hostsobject){
+		for(var group in hostsobject){
+			var hosts = hostsobject[group];
+			for(var i=0;i<hosts.length;i++){
+				var host = hosts[i];
+				if(domain == host.domain) host.disabled = true;
+			}
+		}
+		return hostsobject;
+	},
 	//读取host内容并解析成一个json对象
 	get: function() {
 		var hostsstr = fs.readFileSync(HOSTS, 'utf-8');
@@ -53,163 +64,165 @@ hosts.prototype = {
 		var lines = hostsstr.split(EOL);
 		var hostsobject = {};
 		var currentName;
-        var currentLog = [];
-        var uniq = {};
-        notes = {};
+		var currentLog = [];
+		var uniq = {};
+		notes = {};
 		for (var i = 0; i < lines.length; i++) {
 			var line = this._parseLine(lines[i]);
-            if(!line) continue;
-            var type = line.type;
-            var val = line.value;
-            switch(type){
-                case 1:
-                    if(!currentName){
-                        currentName = defaultName;
-                        hostsobject[currentName] = [];
-                        if(currentLog.length > 0){
-                            notes[currentName] = currentLog.join(EOL);
-                            currentLog = [];
-                        }
-                    }
-                    val.forEach(function (item){
-                        var _item = uniq[currentName + item.domain + item.ip];
-                        if(!_item){
-                            hostsobject[currentName].push(item);
-                            _item = uniq[currentName + item.domain + item.ip] = item;
-                        }
-                        if(currentLog.length > 0){
-                            _item.note = (_item.note ? (_item.note + EOL) : '') + currentLog.join(EOL);
-                        }
-                        
-                    });
-                    currentLog = [];
-                    break;
-                case 2:
-                    currentLog.push(val);
-                    break;
-                case 3:
-                    currentName = val;
-                    hostsobject[currentName] = [];
-                    if(currentLog.length > 0){
-                        notes[currentName] = currentLog.join(EOL);
-                        currentLog = [];
-                    }
-                    break;
-                default:
-                    break;
-            }
+			if (!line) continue;
+			var type = line.type;
+			var val = line.value;
+			switch (type) {
+			case 1:
+				if (!currentName) {
+					currentName = defaultName;
+					hostsobject[currentName] = [];
+					if (currentLog.length > 0) {
+						notes[currentName] = currentLog.join(EOL);
+						currentLog = [];
+					}
+				}
+				val.forEach(function(item) {
+					var _item = uniq[currentName + item.domain + item.ip];
+					if (!_item) {
+						hostsobject[currentName].push(item);
+						_item = uniq[currentName + item.domain + item.ip] = item;
+					}
+					if (currentLog.length > 0) {
+						_item.note = (_item.note ? (_item.note + EOL) : '') + currentLog.join(EOL);
+					}
+
+				});
+				currentLog = [];
+				break;
+			case 2:
+				currentLog.push(val);
+				break;
+			case 3:
+				currentName = val;
+				hostsobject[currentName] = [];
+				if (currentLog.length > 0) {
+					notes[currentName] = currentLog.join(EOL);
+					currentLog = [];
+				}
+				break;
+			default:
+				break;
+			}
 		}
-        if(currentLog.length > 0){
-            notes.bottom = currentLog.join(EOL);
-            currentLog = [];
-        }
+		if (currentLog.length > 0) {
+			notes.bottom = currentLog.join(EOL);
+			currentLog = [];
+		}
 		return hostsobject;
 	},
 	//初始化hosts文件,生成默认分组
 	format: function() {
 		this._batchHost();
 	},
-    //设置一个domain
+	//设置一个domain
 	set: function(domain, ip, options) {
-        var defaultName = this.defaultName;
-        var updateHost = function (host){
-            host.domain = domain;
-            host.ip = ip;
-            typeof options.disabled !== 'undefined' ? host.disabled = options.disabled : '';
-            typeof options.note !== 'undefined' ? host.note = options.note :  '';
-        };
-        ip = ip || '127.0.0.1';
-        options = options || {};
+		var self = this;
+		var defaultName = this.defaultName;
+		var updateHost = function(host) {
+			host.domain = domain;
+			host.ip = ip;
+			typeof options.disabled !== 'undefined' ? host.disabled = options.disabled: '';
+			typeof options.note !== 'undefined' ? host.note = options.note: '';
+		};
+		ip = ip || '127.0.0.1';
+		options = options || {};
 		this._batchHost(function(hostsobject) {
 			groupName = options.groupName || defaultName;
 			var group = hostsobject[groupName] = hostsobject[groupName] || [];
-            for(var i = 0; i < group.length; i++){
-                var host = group[i];
-                if((host.domain === domain && host.ip === ip) || (host.domain === options.olddomain && host.ip === options.oldip)){
-                    updateHost(host);
-                    return hostsobject;
-                }
-            }
-            group.push({
-                    ip: ip,
-                    domain: domain,
-                    disabled: !!options.disabled,
-                    note: options.note||''
-                });
+			for (var i = 0; i < group.length; i++) {
+				var host = group[i];
+				if ((host.domain === domain && host.ip === ip) || (host.domain === options.olddomain && host.ip === options.oldip)) {
+					if(!options.disabled) self._disableAllDomain(domain,hostsobject);
+					updateHost(host);
+					return hostsobject;
+				}
+			}
+			group.push({
+				ip: ip,
+				domain: domain,
+				disabled: !! options.disabled,
+				note: options.note || ''
+			});
 			return hostsobject;
 		});
 	},
-    //删除一个host
+	//删除一个host
 	remove: function(domain, ip, groupName) {
-        groupName = groupName||this.defaultName;
+		groupName = groupName || this.defaultName;
 		this._batchHost(function(hostsobject) {
 			var group = hostsobject[groupName];
 			if (group) {
-                var _group = [];
-                for(var i = 0; i < group.length; i++){
-                    var host = group[i];
-                    if(host.domain === domain && host.ip === ip) continue;
-                    _group.push(host);
-                }
-                if(_group.length === group.length) return;
-                hostsobject[groupName] = _group;
-                return hostsobject;
+				var _group = [];
+				for (var i = 0; i < group.length; i++) {
+					var host = group[i];
+					if (host.domain === domain && host.ip === ip) continue;
+					_group.push(host);
+				}
+				if (_group.length === group.length) return false;
+				hostsobject[groupName] = _group;
+				return hostsobject;
 			}
 		});
 	},
-    //添加分组
+	//添加分组
 	addGroup: function(groupName, log) {
-        if(log){
-            notes[groupName] = log;
-        }
+		if (log) {
+			notes[groupName] = log;
+		}
 		this._batchHost(function(hostsobject) {
-			if (hostsobject[groupName]) return;
-            hostsobject[groupName] = [];
+			if (hostsobject[groupName]) return false;
+			hostsobject[groupName] = [];
 			return hostsobject;
 		});
 	},
-    //删除分组
+	//删除分组
 	removeGroup: function(groupName) {
 		this._batchHost(function(hostsobject) {
-			if (hostsobject[groupName]){
-                delete hostsobject[groupName];
-                return hostsobject;
-            }
-		});
-	},
-    //修改分组名
-	setGroup: function(oldName, newName) {
-		this._batchHost(function(hostsobject) {
-            if (hostsobject[oldName] && !hostsobject[newName]) {
-                hostsobject[newName] = hostsobject[oldName];
-                delete hostsobject[oldName];
-                return hostsobject;
+			if (hostsobject[groupName]) {
+				delete hostsobject[groupName];
+				return hostsobject;
 			}
 		});
 	},
-    //将一条host从一个组移动到另一个组
+	//修改分组名
+	setGroup: function(oldName, newName) {
+		this._batchHost(function(hostsobject) {
+			if (hostsobject[oldName] && ! hostsobject[newName]) {
+				hostsobject[newName] = hostsobject[oldName];
+				delete hostsobject[oldName];
+				return hostsobject;
+			}
+		});
+	},
+	//将一条host从一个组移动到另一个组
 	move: function(domain, ip, groupName, target_groupName) {
 		this._batchHost(function(hostsobject) {
 			var group = hostsobject[groupName];
 			var t_group = hostsobject[target_groupName] = hostsobject[target_groupName] || [];
 			if (group) {
-                var move, i;
-                //目标分组里是否存在该条host，若存在则不作处理。
-                for(i = 0; i < t_group.length; i++){
-                    var host = t_group[i];
-                    if(host.domain === domain && host.ip === ip){
-                        return;
-                    }
-                }
-                for(i = 0; i < group.length; i++){
-                    var host = group[i];
-                    if(host.domain === domain && host.ip === ip){
-                        t_group.push(host);
-                        group.splice(i, 1);
-                        break;
-                    }
-                }
-                return hostsobject;
+				var host, move, i;
+				//目标分组里是否存在该条host，若存在则不作处理。
+				for (i = 0; i < t_group.length; i++) {
+					host = t_group[i];
+					if (host.domain === domain && host.ip === ip) {
+						return false;
+					}
+				}
+				for (i = 0; i < group.length; i++) {
+					host = group[i];
+					if (host.domain === domain && host.ip === ip) {
+						t_group.push(host);
+						group.splice(i, 1);
+						break;
+					}
+				}
+				return hostsobject;
 			}
 		});
 	},
@@ -222,20 +235,23 @@ hosts.prototype = {
 		this.setDomainDisabled(domain, ip, groupName || defaultName, 0);
 	},
 	setDomainDisabled: function(domain, ip, groupName, disabled) {
+		var self = this;
 		this._batchHost(function(hostsobject) {
+			if(!disabled) self._disableAllDomain(domain,hostsobject);
 			var group = hostsobject[groupName];
 			if (group) {
-                for(var i = 0; i < group.length; i++){
-                    var host = group[i];
-                    if(host.domain === domain && host.ip === ip){
-                        if(host.disabled == disabled){
-                            return;
-                        }
-                        host.disabled = disabled;
-                        return hostsobject;
-                    }
-                }
+				for (var i = 0; i < group.length; i++) {
+					var host = group[i];
+					if (host.domain === domain && host.ip === ip) {
+						if (host.disabled == disabled) {
+							return false;
+						}
+						host.disabled = disabled;
+						return hostsobject;
+					}
+				}
 			}
+			return false;
 		});
 	},
 	//注释一个组
@@ -247,77 +263,80 @@ hosts.prototype = {
 		this.setGroupDisabled(groupName, 0);
 	},
 	setGroupDisabled: function(groupName, disabled) {
+		var self = this;
 		this._batchHost(function(hostsobject) {
 			var group = hostsobject[groupName];
-            if(group){
-                for(var i = 0; i < group.length; i++){
-                    var host = group[i];
-                    host.disabled = disabled;
-                }
-                return hostsobject;
-            }
+			if (group) {
+				for (var i = 0; i < group.length; i++) {
+					var host = group[i];
+					if(!disabled) self._disableAllDomain(host.domain,hostsobject);
+					host.disabled = disabled;
+				}
+				return hostsobject;
+			}
 		});
 	},
-    _parseLine: function (line){
-        var obj;
-        var line = line.trim();
-        if(line){
-            var isGroupLine = line.match(groupReg);
-            obj = {};
-            if(isGroupLine){
-                obj.type = 3;
-                obj.value = isGroupLine[1];
-            }else{
-                var line2 = line.split(blankReg);
-                if(line2.length < 2 || !line2[0].match(/^\d|#[\d]/g)){
-                    obj.type = 2;
-                    obj.value = line;
-                }else{
-                    var ip = line2.shift();
-                    var domains = line2;
-                    var disabled = (/^#/).test(ip);
-                    if(disabled){
-                        ip = ip.slice(1);
-                    }
-                    obj.type = 1;
-                    obj.value = [];
-                    domains.forEach(function (domain){
-                        obj.value.push({
-                            ip: ip,
-                            domain: domain,
-                            disabled: disabled
-                        });
-                    });
-                }
-            }
-        }
-        return obj;
-    },
+	_parseLine: function(line) {
+		var obj;
+		line = line.trim();
+		if (line) {
+			var isGroupLine = line.match(groupReg);
+			obj = {};
+			if (isGroupLine) {
+				obj.type = 3;
+				obj.value = isGroupLine[1];
+			} else {
+				var line2 = line.split(blankReg);
+				if (line2.length < 2 || ! line2[0].match(/^\d|#[\d]/g)) {
+					obj.type = 2;
+					obj.value = line;
+				} else {
+					var ip = line2.shift();
+					var domains = line2;
+					var disabled = (/^#/).test(ip);
+					if (disabled) {
+						ip = ip.slice(1);
+					}
+					obj.type = 1;
+					obj.value = [];
+					domains.forEach(function(domain) {
+						obj.value.push({
+							ip: ip,
+							domain: domain,
+							disabled: disabled
+						});
+					});
+				}
+			}
+		}
+		return obj;
+	},
 	_batchHost: function(fn) {
 		var hostsobject = this.get();
 		hostsobject = fn ? fn(hostsobject) : hostsobject;
-        if(!hostsobject) return;
+		if (!hostsobject) return;
 		var linesStr = this._hostTostr(hostsobject);
 		fs.writeFileSync(HOSTS, linesStr);
 	},
 	_hostTostr: function(hostobj) {
 		var lines = [];
 		for (var i in hostobj) {
-            var group = hostobj[i];
-            notes[i] ? lines.push(notes[i]) : '';
+			var group = hostobj[i];
+			notes[i] ? lines.push(notes[i]) : '';
 			lines.push('## @' + i);
-            for(var k = 0; k < group.length; k++){
-                var host = group[k];
-                var line = host.disabled ? "#" : '';
-                host.note ? lines.push(host.note) : '';
-                line += host.ip + ' ' + host.domain;
-                lines.push(line);
-            }
-            lines.push("");
+			for (var k = 0; k < group.length; k++) {
+				var host = group[k];
+				var line = host.disabled ? "#": '';
+				host.note ? lines.push(host.note) : '';
+				line += host.ip + ' ' + host.domain;
+				lines.push(line);
+			}
+			lines.push("");
 		}
-        notes.bottom ? lines.push(notes.bottom) : '';
+		notes.bottom ? lines.push(notes.bottom) : '';
 		return lines.join(EOL);
 	}
 };
 
 module.exports = new hosts();
+
